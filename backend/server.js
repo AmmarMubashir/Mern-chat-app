843acf2650e6433182f5ade49aefcb9f2601f8f9
Ammar <muhammadammarmubashir@gmail.com>
2024-03-30 15:26:32 +0500

    Backend completed

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
+
+import connectToMongodb from "./db/connectToMongodb.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming request with JSON payload
+app.use(cookieParser());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/messages", messageRoutes);
+app.use("/api/v1/users", userRoutes);
+
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("HI");
+// });
+
+app.listen(PORT, () => {
+  connectToMongodb();
+  console.log(`App running on port ${PORT}`);
+});
